9ba3902ee299d106e634511d47a7b74b93d69139
Kanat200 <mkanat23.2004@gmail.com>
2024-10-21 16:36:39 +0600

    Initial commit of Spring Core

--- src/main/java/com/KanatSpringCourse/RockMusic.java
+++ src/main/java/com/KanatSpringCourse/RockMusic.java
@@ -1,12 +1,28 @@
 package com.KanatSpringCourse;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Scope("prototype")
+
 public class RockMusic implements Music{
+
+    @PostConstruct
+    public void doMyInt() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     private List<String> rockSongs = new ArrayList<>();
 
     // Блок инициализации объекта (англ. Instance initialization block)
